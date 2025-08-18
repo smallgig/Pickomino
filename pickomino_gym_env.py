f00bc01c70aeb2466a4d181c6da2e370f006da52
Jarl <jarl@hoyem.de>
2025-08-18 19:45:51 +0200

    White space, comments and docstrings ONLY! This commit should have no effect on the code.

--- pickomino_gym_env.py
+++ pickomino_gym_env.py
@@ -1,40 +1,41 @@
+"""Pickomino game with gymnasium API."""
 from typing import Optional
 import numpy as np
 import gymnasium as gym
 import random as rand
 
-from markdown.extensions.smarty import remainingDoubleQuotesRegex
-from torchrl.envs import terminated_or_truncated
-
 
 class PickominoEnv(gym.Env):
 
     def __init__(self, num_players):
+        """Constructor."""
         self.num_dice = 8
         self.remaining_dice = self.num_dice
         # Define what the agent can observe
         # Dict space gives us structured, human-readable observations
         self._dices_collected = np.array([0, 0, 0, 0, 0, 0])
-        self._dices_rolled = np.array([0, 0, 0, 0, 0 ,0])
+        self._dices_rolled = np.array([0, 0, 0, 0, 0, 0])
         self.roll_counter = 0
-
-
         self.observation_space = gym.spaces.Dict(
             {
-                "dices_collected": gym.spaces.Discrete(n=6, start=0),
-                "dices_rolled": gym.spaces.Discrete(n=6,start=0),
-                "player": gym.spaces.Discrete(num_players), # Players in game
-                # "tiles": gym.spaces.Discrete(n=16, start=21) # Tiles that can be taken
+                "dices_collected": gym.spaces.Discrete(n=6),
+                "dices_rolled": gym.spaces.Discrete(n=6),
+                "player": gym.spaces.Discrete(num_players),  # Number of players in the game.
+                # "tiles": gym.spaces.Discrete(n=16, start=21) # Tiles that can be taken.
             }
         )
-        self.action_space = gym.spaces.Tuple((gym.spaces.Discrete(6), gym.spaces.Discrete(2))) # First Action which dice u take,
-                                                                             # Second Action Roll again or not
+        # Action space is a tuple. First action: which dice you take. Second action: roll again or not.
+        self.action_space = gym.spaces.Tuple((gym.spaces.Discrete(6), gym.spaces.Discrete(2)))
 
     def _get_sum(self, collected_dices):
+        """Return the sum of the collected dices."""
         return_value = 0
+
+        # Dice with eyes = number of eyes per dice times the number of dices.
         for ind, dice in enumerate(collected_dices):
             return_value += dice * ind
 
+        # Worms have value five.
         return_value = return_value + collected_dices[0] * 5
 
         return return_value
@@ -43,7 +44,7 @@ class PickominoEnv(gym.Env):
         """Convert internal state to observation format.
 
         Returns:
-            dict: Observation with agent and target positions
+            tuple: Dices collected and dices rolled.
         """
         return self._dices_collected, self._dices_rolled
 
@@ -51,15 +52,16 @@ class PickominoEnv(gym.Env):
         """Convert internal state to observation format.
 
         Returns:
-            dict: Observation with agent and target positions
+            dict: Tiles distribution
         """
+        # TODO: implement correct return value
         return self.observation_space
 
     def _get_info(self):
         """Compute auxiliary information for debugging.
 
         Returns:
-            dict: Info with distance between agent and target
+            dict: Info with additional information which is useful for debugging but not necessary for learning.
         """
         pass
 
@@ -76,9 +78,8 @@ class PickominoEnv(gym.Env):
         # IMPORTANT: Must call this first to seed the random number generator
         super().reset(seed=seed)
 
-        # Randomly place the agent anywhere on the grid
         self._dices_collected = np.array([0, 0, 0, 0, 0, 0])
-        dice_list = [0, 1, 2, 3, 4, 5]
+
         for i in range(self.num_dice):
             self._dices_rolled[rand.randint(0, 5)] += 1
 
@@ -89,6 +90,7 @@ class PickominoEnv(gym.Env):
         return observation, info
 
     def legal_move(self, action):
+        """Check if action is allowed."""
         terminated = False
         truncated = False
 
@@ -99,16 +101,16 @@ class PickominoEnv(gym.Env):
 
         return terminated, truncated
 
-
     def step(self, action):
-        """Execute one timestep within the environment.
+        """Execute one roll of the dices and picking or returning a tile.
 
         Args:
-            action: The action to take (0-3 for directions)
+            action: The action to take: which dice to collect.
 
         Returns:
             tuple: (observation, reward, terminated, truncated, info)
         """
+        # TODO: currently only the roll of the dices is implemented. The tile moving phase is not implemented yet.
         terminated, truncated = self.legal_move(action)
 
         if terminated or truncated:
@@ -116,9 +118,8 @@ class PickominoEnv(gym.Env):
             self._dices_rolled = np.array([0, 0, 0, 0, 0, 0])
             self.roll_counter = 0
 
-
         self._dices_collected[action[0]] = self._dices_rolled[action[0]]
-        self._dices_rolled = np.array([0, 0, 0, 0, 0 ,0])
+        self._dices_rolled = np.array([0, 0, 0, 0, 0, 0])
 
         if action[1]:
             for i in range(self.remaining_dice):
@@ -133,6 +134,7 @@ class PickominoEnv(gym.Env):
         self.roll_counter += 1
 
         return observation, reward, terminated, truncated, info
+
 
 if __name__ == "__main__":
     env = PickominoEnv(2)
@@ -156,4 +158,3 @@ if __name__ == "__main__":
     print(observation, reward)
     observation, reward, _, _, _ = env.step(action=(5, 1))
     print(observation, reward)
-
