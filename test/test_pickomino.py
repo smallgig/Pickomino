c9d97de31a81f0a1a4ae6fc2f81c3e188d47c69f
Jarl <jarl@hoyem.de>
2025-10-28 17:43:52 +0100

    - Only started this issue. But want to merge now to run pre-commit --all-files after excluding test files. - test_pickomino.py runs and could be automated as pre-requisite for git actions.

--- test/test_pickomino.py
+++ test/test_pickomino.py
@@ -1,47 +1,57 @@
 """Test Pickomino."""
 
 import numpy as np
-import pickomino_gym_env
+from pickomino_env.pickomino_gym_env import PickominoEnv
 
-
-def test_basics():
-    action = (0, 1)
-    observation, reward, terminated, truncated, info = env.step(action)
-
-    assert True
-
-
-def test_reset():
-    observation, info = env.reset()
-    print(observation)
-
-    assert np.array_equal(observation[0], np.array([0, 0, 0, 0, 0, 0]))
-    assert np.array_equal(observation[1], np.array([0, 2, 1, 4, 1, 0]))
-
-
-def test_multiple_actions():
-    observation, info = env.reset()
-
-    taken = []
-    for step in range(6):
-        selection = int(np.argmax(observation[1]))
-
-        # Do not select the same die face value again
-        if selection in taken:
-            observation[1][selection] = 0
-            selection = int(np.argmax(observation[1]))
-            observation, reward, terminated, truncated, info = env.step(selection)
+# Create environment to test.
+env = PickominoEnv(2)
 
 
 def test__init__():
     """Test init function."""
+    assert env._number_of_bots == 2
+
+
+def test_reset():
+    """Test reset function."""
+    observation, info = env.reset()
+    assert np.array_equal(observation["dice_collected"], np.array([0, 0, 0, 0, 0, 0]))
+    assert sum(observation["dice_rolled"]) == 8
+
+
+def test_step():
+    """Test step function"""
+    observation, info = env.reset()
+    die_face_to_collect = np.argmax(observation["dice_rolled"])
+    action = (die_face_to_collect, 0)  # Collect and keep rolling.
+    observation, reward, terminated, truncated, info = env.step(action)
+    assert sum(observation["dice_collected"]) + sum(observation["dice_rolled"]) == 8
+
+
+def test_multiple_actions():
+    """Test multiple actions."""
+    pass
+    # observation, info = env.reset()
+    #
+    # taken = []
+    # for step in range(6):
+    #     selection = int(np.argmax(observation['dice_rolled']))
+    #
+    #     # Do not select the same die face value again
+    #     if selection in taken:
+    #         observation['dice_collected'][selection] = 0
+    #         selection = int(np.argmax(observation['dice_rolled']))
+    #         observation, reward, terminated, truncated, info = env.step((selection, 0))
 
 
 def run_all_test():
     """Run all tests."""
     test__init__()
+    test_reset()
+    test_step()
+    test_multiple_actions()
 
 
 if __name__ == "__main__":
-    env = pickomino_gym_env.PickominoEnv(2)
+    env = PickominoEnv(2)
     run_all_test()
