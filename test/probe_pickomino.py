f0dd9a9d22cae9eab15697ee714668d3530935ac
RobMeetsAI <raithelrobin@gmail.com>
2025-10-29 09:52:45 +0100

    #88

--- test/probe_pickomino.py
+++ test/probe_pickomino.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 
 import pickomino_env  # wichtig: löst das register() aus
 
-env = gym.make("Pickomino-v0")
+env = gym.make("Pickomino-v0", number_of_bots=2)
 obs, info = env.reset()
 print(obs)
 terminated = False
