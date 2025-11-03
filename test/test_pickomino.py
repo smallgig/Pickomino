2d237bd3907865bc69340a98274781a908f3148c
Jarl <jarl@hoyem.de>
2025-11-03 16:40:57 +0100

    Usefull stuff from pickominio_baseline.py copied to test_pickomino.py and deleted pickominio_baseline.py.

--- test/test_pickomino.py
+++ test/test_pickomino.py
@@ -1,17 +1,22 @@
 """Test Pickomino."""
 
-import gymnasium as gym
+import time
 import numpy as np
+import gymnasium as gym
+import matplotlib.pyplot as plt
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 import pickomino_env  # Important: activates the registration.
 
+log_dir = "./logs/"
+run_time = 0
+
 # Create environment to test.
-env = gym.make("Pickomino-v0", number_of_bots=2)
-
-
-# def test__init__():
-#     """Test init function."""
-#     assert env._number_of_bots == 2
+env = gym.make("Pickomino-v0", number_of_bots=2)  # base environment
 
 
 def test_reset():
@@ -50,6 +55,54 @@ def test_multiple_actions():
     #         observation['dice_collected'][selection] = 0
     #         selection = int(np.argmax(observation['dice_rolled']))
     #         observation, reward, terminated, truncated, info = env.step((selection, 0))
+
+
+def test_stable_baselines3():
+    """Test environment works correctly with Stable Baselines3."""
+    check_env(env)
+
+
+def test_ppo():
+    """Test PPO from stable_baselines3."""
+    start_time = time.time()
+
+    def make_env():
+        """Create environment with function for testing PPO."""
+        ppo_env = gym.make("Pickomino-v0", number_of_bots=2)
+        ppo_env = Monitor(ppo_env, log_dir)  # Wrap with Stable Baselines 3's Monitor.
+        return ppo_env
+
+    # Vectorize environment for PPO for parallel environments
+    vec_ppo_env = DummyVecEnv([make_env])
+
+    algorithm = PPO("MultiInputPolicy", vec_ppo_env)  # Add ', verbose=1' as necessary
+    algorithm.learn(total_timesteps=100)  # 1 step = on action! (not episode!)
+
+    end_time = time.time()
+    global run_time
+    run_time = end_time - start_time
+
+
+def test_ppo_plotting():
+    """Test PPO plotting."""
+    x, y = ts2xy(load_results(log_dir), "timesteps")
+    plt.plot(x, y)
+    plt.xlabel("Timesteps")
+    plt.ylabel("Rewards")
+    plt.title(f"Learning Curve in {run_time:.0f} seconds")
+    plt.grid()
+    plt.show()
+
+
+# def test_ppo_parallel():
+#    """Test PPO parallel."""
+#    start_time = time.time()
+# from stable_baselines3.common.vec_env import SubprocVecEnv
+
+# def make_env():
+#     return gym.make("CartPole-v1")
+
+# env = SubprocVecEnv([make_env for _ in range(8)])  # 8 parallel envs
 
 
 if __name__ == "__main__":
