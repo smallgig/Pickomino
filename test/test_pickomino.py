4c53e724c1ccaad4f8654a5964254e210226682f
Jarl <jarl@hoyem.de>
2025-11-03 14:59:49 +0100

    Usefull stuff from probe_pickomino.py copied to test_pickomino.py and deleted probe_pickomino.py

--- test/test_pickomino.py
+++ test/test_pickomino.py
@@ -1,15 +1,17 @@
 """Test Pickomino."""
 
+import gymnasium as gym
 import numpy as np
-from pickomino_env.pickomino_gym_env import PickominoEnv
+
+import pickomino_env  # Important: activates the registration.
 
 # Create environment to test.
-env = PickominoEnv(2)
+env = gym.make("Pickomino-v0", number_of_bots=2)
 
 
-def test__init__():
-    """Test init function."""
-    assert env._number_of_bots == 2
+# def test__init__():
+#     """Test init function."""
+#     assert env._number_of_bots == 2
 
 
 def test_reset():
@@ -17,6 +19,8 @@ def test_reset():
     observation, info = env.reset()
     assert np.array_equal(observation["dice_collected"], np.array([0, 0, 0, 0, 0, 0]))
     assert sum(observation["dice_rolled"]) == 8
+    assert np.all(observation["tiles_table"] == 1)  # Initially [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    assert np.all(observation["tile_players"] == 0)  # Initially [0, 0, 0]
 
 
 def test_step():
@@ -26,6 +30,10 @@ def test_step():
     action = (die_face_to_collect, 0)  # Collect and keep rolling.
     observation, reward, terminated, truncated, info = env.step(action)
     assert sum(observation["dice_collected"]) + sum(observation["dice_rolled"]) == 8
+    assert reward == 0
+    assert not terminated
+    assert not truncated
+    assert info["player_stack"] == [42]  # 42 is initial invalid value for testing.
 
 
 def test_multiple_actions():
