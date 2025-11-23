27b76d689013d44d6f9a7cf65ed9c90081465a6c
Jarl <jarl@hoyem.de>
2025-11-23 11:19:08 +0100

    Added named constants for rendering.

--- pickomino_env/modules/constants.py
+++ pickomino_env/modules/constants.py
@@ -14,6 +14,15 @@ __all__ = [
     "ACTION_INDEX_DICE",
     "RENDER_MODE_HUMAN",
     "RENDER_MODE_RGB_ARRAY",
+    "WINDOW_WIDTH",
+    "WINDOW_HEIGHT",
+    "BACKGROUND_COLOR",
+    "TILE_WIDTH",
+    "TILE_HEIGHT",
+    "TILES_PER_ROW",
+    "TILE_SPACING",
+    "TILES_START_X",
+    "TILES_START_Y",
 ]
 
 from typing import Final
@@ -35,6 +44,21 @@ ACTION_INDEX_ROLL: Final[int] = 1
 ACTION_ROLL: Final[int] = 0
 ACTION_STOP: Final[int] = 1
 
-# Rendering constants.
+# Rendering mode constants.
 RENDER_MODE_HUMAN: Final[str] = "human"
 RENDER_MODE_RGB_ARRAY: Final[str] = "rgb_array"
+
+# Rendering window dimensions.
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Rendering background color (RGB).
+BACKGROUND_COLOR = (70, 130, 70)  # Nice green.
+
+# Tile rendering
+TILE_WIDTH = 80
+TILE_HEIGHT = 160
+TILES_PER_ROW = 8
+TILE_SPACING = 10
+TILES_START_X = 50
+TILES_START_Y = 250
