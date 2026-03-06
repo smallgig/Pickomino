b51b3d2bb229f5bdc21e1d6e034df3d90b2841b1
Jarl <jarl@hoyem.de>
2026-03-06 18:06:04 +0100

    Update logging_config.py
    Move comment above line because line was too long.

--- pickomino_env/modules/logging_config.py
+++ pickomino_env/modules/logging_config.py
@@ -9,7 +9,8 @@ try:  # pylint: disable=too-many-try-statements
     from loguru import logger
 
     logger.remove()  # Remove default stderr handler.
-    logger.add(Path(tempfile.gettempdir()) / "pickomino.log", mode="w")  # On Windows: C:\Users\<username>\AppData\Local\Temp\pickomino.log
+    # On Windows: C:\Users\<username>\AppData\Local\Temp\pickomino.log.
+    logger.add(Path(tempfile.gettempdir()) / "pickomino.log", mode="w")  # See path for log file in line above.
 
     def log(message: str) -> None:
         """Log a debug message if loguru is available."""
