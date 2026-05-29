3d7a89765eb8782d06a28fa6584b77968787ef8d
RobMeetsAI <raithelrobin@gmail.com>
2026-05-29 18:50:15 +0200

    Deleted redundant ignore.

--- pickomino_env/pickomino.py
+++ pickomino_env/pickomino.py
@@ -196,7 +196,7 @@ class PickominoEnv(gym.Env):  # type: ignore[type-arg]
         self.render_mode = render_mode
         self._renderer = Renderer(self.render_mode)
 
-    def render(  # type: ignore[override] # pyright: ignore[reportIncompatibleMethodOverride]
+    def render(  # type: ignore[override]
         self,
     ) -> NDArray[np.uint8] | None:
         """Render the current game state.
